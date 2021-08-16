901df0723e6cdbe5712405932633f2819795fd14
Ula Buczak <mankowskau@gmail.com>
2021-08-16 23:33:10 +0200

    Refactoring code

--- src/js/Components/colors.js
+++ src/js/Components/colors.js
@@ -7,12 +7,15 @@ export class Colors {
     this.getElements();
     this.renderColor();
     this.initSliders();
+    this.initCopyClipboard();
   }
 
   getElements(){
     this.colorDivs = document.querySelectorAll('.color');
     this.sliders = document.querySelectorAll('input[type=range]');
     this.initialsColors;
+    this.currentHexes = document.querySelectorAll('.color h2');
+    this.popUp = document.querySelector('.copy-container');
   }
 
   generateHex(){
@@ -42,10 +45,10 @@ export class Colors {
       const hue = sliders[0],
         brightness = sliders[1],
         saturation = sliders[2];
-
-
       this.colorizeSliders(color, hue, brightness, saturation);
     });
+
+    this.resetInputs();
   }
 
   checkContrast(color, text){
@@ -96,6 +99,8 @@ export class Colors {
           .set('hsl.l', brightness.value)
           .set('hsl.h', hue.value);
         this.colorDivs[index].style.backgroundColor = color;
+
+        this.colorizeSliders(color, hue, saturation, brightness);
       });
     });
 
@@ -106,25 +111,25 @@ export class Colors {
     });
   }
 
-  // updateInputValue (e) {
-  //   const index = e.target.getAttribute('data-bright')|| e.target.getAttribute('data-hue')|| e.target.getAttribute('data-sat');
+  initCopyClipboard(){
+    this.currentHexes.forEach(hex =>{
+      hex.addEventListener('click', ()=>{
+        this.copyToClipBoard(hex, this.popUp);
+      });
+    });
+  }
 
-  //   let sliders = e.target.parentElement.querySelectorAll('input[type="range"]');
-
-  //   const hue = sliders[0];
-  //   const brightness = sliders[1];
-  //   const saturation = sliders[2];
-
-    
-  //   const bgColor = this.initialsColors[index];
-
-  //   let color = chroma(bgColor)
-  //     .set('hsl.s', saturation.value)
-  //     .set('hsl.l', brightness.value)
-  //     .set('hsl.h', hue.value);
-
-  //   this.colorDivs[index].style.backgroundColor = color;
-  // }
+  copyToClipBoard(hex, popUp){
+    const el = document.createElement('textarea');
+    el.value = hex.innerText;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    const popUpBox = popUp.children[0];
+    popUp.classList.add('active');
+    popUpBox.classList.add('active');
+  }
 
   updateTextUi (index) {
     const activeDiv = this.colorDivs[index];
@@ -138,6 +143,27 @@ export class Colors {
     for(let icon of icons){
       this.checkContrast(color, icon);
     }
+  }
+
+  resetInputs(){
+    const sliders = document.querySelectorAll('.sliders input');
+    sliders.forEach(slider =>{
+      if(slider.name === "hue"){
+        const hueColor = this.initialsColors[slider.getAttribute("data-hue")];
+        const hueValue = chroma(hueColor).hsl()[0];
+        slider.value = Math.floor(hueValue);
+      }
+      if(slider.name ==="brighntess"){
+        const brightColor = this.initialsColors[slider.getAttribute("data-bright")];
+        const brightValue = chroma(brightColor).hsl()[2];
+        slider.value = (Math.floor(brightValue * 100)/100);
+      }
+      if(slider.name ==="saturation"){
+        const satColor = this.initialsColors[slider.getAttribute("data-sat")];
+        const satValue = chroma(satColor).hsl()[1];
+        slider.value = (Math.floor(satValue * 100)/100);
+      }
+    });
   }
 
 }
